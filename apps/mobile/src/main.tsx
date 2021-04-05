8d71c6604ce02f6c1f14d8714f3cff0a1f7f5e6e
JoaoPauloCMarra <contato@webnexus.com.br>
2021-04-05 19:27:33 +0200

    fixing android connection to local server

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import { ApolloWrapper } from '@nx-fullstack/ui-components';
 
 import App from './app/App';
 
+const host = Platform.OS === 'ios' ? 'localhost' : '10.0.2.2';
+
 const Main = () => (
-  <ApolloWrapper uri="http://localhost:5000/graphql">
+  <ApolloWrapper uri={`http://${host}:5000/graphql`}>
     <App />
   </ApolloWrapper>
 );
